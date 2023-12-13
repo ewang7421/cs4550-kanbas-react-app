7c4901b75c52e1df2136144e4816540610a4840d
Eric Wang <ewang7412@gmail.com>
2023-12-13 16:49:15 -0500

    fix env

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -18,7 +18,7 @@ function Kanbas() {
     startDate: "2023-09-10",
     endDate: "2023-12-15",
   });
-  const URL = "http://localhost:4000/api/courses";
+  const URL = process.env.REACT_APP_BASE_API_URL;
   const findAllCourses = async () => {
     const response = await axios.get(URL);
     setCourses(response.data);
